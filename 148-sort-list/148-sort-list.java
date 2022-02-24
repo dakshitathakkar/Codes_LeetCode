6c872af82b81b56da3343e615fbf4a2d4f013ee0
Dakshita Thakkar <55482291+dakshitathakkar@users.noreply.github.com>
2022-02-24 10:49:14 +0530

    Time: 12 ms (37.85%), Space: 57.2 MB (20.88%) - LeetHub

---
+++ 148-sort-list/148-sort-list.java
@@ -0,0 +1,76 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode prev = head;
+        while(fast != null && fast.next != null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+        
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(slow);
+        return mergeTwoSort(l1,l2);
+    }
+    
+    // public ListNode midNode(ListNode head){
+    //     if(head == null || head.next == null)
+    //         return head;
+    //     ListNode fast = head;
+    //     ListNode slow = head;
+    //     while(fast != null && fast.next != null){
+    //         slow = slow.next;
+    //         fast = fast.next.next;
+    //     }
+    //     return slow;
+    // }
+    
+    public ListNode mergeTwoSort(ListNode h1, ListNode h2){
+        if(h1 == null || h2 == null)
+            return h1==null?h1:h2;
+        ListNode h3 = new ListNode(-1);
+        ListNode p3 = h3;
+        ListNode p1 = h1;
+        ListNode p2 = h2;
+        
+        while(p1 != null && p2 != null){
+            if(p1.val < p2.val){
+                p3.next = p1;
+                p1 = p1.next;
+                p3 = p3.next;
+            }
+            else{
+                p3.next = p2;
+                p2 = p2.next;
+                p3 = p3.next;
+            }
+        }
+        while(p1 != null){
+            p3.next = p1;
+            p1 = p1.next;
+            p3 = p3.next;
+        }
+        
+        while(p2 != null){
+            p3.next = p2;
+            p2 = p2.next;
+            p3 = p3.next;
+        }
+        
+        return h3.next;
+    }
+}
