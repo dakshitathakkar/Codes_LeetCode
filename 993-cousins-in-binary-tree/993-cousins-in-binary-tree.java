555eb52349949cc2c12575053f243f75f6085975
Dakshita Thakkar <55482291+dakshitathakkar@users.noreply.github.com>
2022-02-26 10:55:11 +0530

    Time: 1 ms (59.97%), Space: 42.2 MB (12.14%) - LeetHub

---
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isCousins(TreeNode root, int x, int y) {
+        if(root == null)    return false;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            boolean isX = false;
+            boolean isY = false;
+            
+            for(int i=0; i<size; i++){
+                TreeNode curr = q.poll();
+                if(curr.val == x)   isX = true;
+                if(curr.val == y)   isY = true;
+                
+                if(curr.left != null && curr.right != null){
+                    if(curr.left.val == x && curr.right.val == y)   
+                        return false;
+                    if(curr.left.val == y && curr.right.val == x)
+                        return false;
+                }
+                
+                if(curr.left != null)
+                    q.add(curr.left);
+                if(curr.right != null)
+                    q.add(curr.right);
+            }
+            if(isX && isY)  return true;
+        }
+        return false;
+      
+    }
+      
+}
