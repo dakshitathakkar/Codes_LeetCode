ffcff16ac5ee492eb5813f79bd1a43239ae29297
Dakshita Thakkar <55482291+dakshitathakkar@users.noreply.github.com>
2022-05-02 10:01:18 +0530

    Time: 1 ms (96.61%), Space: 43 MB (91.61%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.java
+++ 905-sort-array-by-parity/905-sort-array-by-parity.java
@@ -1,17 +1,11 @@
 class Solution {
-    public int[] sortArrayByParity(int[] nums) {
-        if(nums.length == 0)
-            return nums;
-        int e = nums[0];
-        int o = nums[0];
-        ArrayList<Integer> res = new ArrayList<>();
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] %2 == 0)
-                res.add(0,nums[i]);
-            else
-                res.add(nums[i]);
-        }
-        int[] arr = res.stream().mapToInt(i -> i).toArray();
-        return arr;
+    public int[] sortArrayByParity(int[] A) {
+        for (int i = 0, j = 0; j < A.length; j++)
+            if (A[j] % 2 == 0) {
+                int tmp = A[i];
+                A[i++] = A[j];
+                A[j] = tmp;;
+            }
+        return A;
     }
 }
