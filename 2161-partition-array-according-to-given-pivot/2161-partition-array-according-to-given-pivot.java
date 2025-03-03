3c5e56342c06173cc2e91292dd1e2812f9f3210d
Dakshita Thakkar <55482291+dakshitathakkar@users.noreply.github.com>
2025-03-03 20:55:06 +0530

    Time: 6 ms (38.63%), Space: 68.1 MB (44.51%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -2,29 +2,19 @@ class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
         int n = nums.length;
         int[] result = new int[n];
-        int j=0;
+        int l=0;
+        int r=n-1;
+        Arrays.fill(result,pivot);
         for(int i=0;i<n;i++){
-            if(nums[i] != Integer.MAX_VALUE && nums[i]<pivot){
-                result[j] = nums[i];
-                nums[i] = Integer.MAX_VALUE;
-                j++;
+            if(nums[i]<pivot){
+                result[l] = nums[i];
+                l++;
+            }
+            if(nums[n-i-1]>pivot){
+                result[r] = nums[n-i-1];
+                r--;
             }
         }
-        for(int i=0;i<n;i++){
-            if(nums[i] != Integer.MAX_VALUE && nums[i]==pivot){
-                result[j] = nums[i];
-                nums[i] = Integer.MAX_VALUE;
-                j++;
-            }
-        }
-        for(int i=0;i<n;i++){
-            if(nums[i] != Integer.MAX_VALUE && nums[i]>pivot){
-                result[j] = nums[i];
-                nums[i] = Integer.MAX_VALUE;
-                j++;
-            }
-        }
-
         return result;
     }
 }
