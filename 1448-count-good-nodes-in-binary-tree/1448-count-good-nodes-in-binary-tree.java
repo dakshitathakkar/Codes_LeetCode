927d6b43acd9afc11bd8c3e036ac251676caaa35
Dakshita Thakkar <55482291+dakshitathakkar@users.noreply.github.com>
2025-04-08 20:02:57 +0530

    Time: 2 ms (85.57%), Space: 54.2 MB (12.52%) - LeetHub

---
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count = 0;
+    public int goodNodes(TreeNode root) {
+        int max = root.val;
+        traverse(root,max);
+        return count;
+    }
+
+    public void traverse(TreeNode root,int max){
+        if(root==null)  return;
+        if(root.val >=max){
+            count++;
+            max = root.val;
+        }
+        traverse(root.left,max);
+        traverse(root.right,max);
+    }
+}
