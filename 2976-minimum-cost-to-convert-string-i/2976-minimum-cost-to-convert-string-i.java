f55d076704e8b920cfe7831e39f45589d19748eb
Dakshita Thakkar <55482291+dakshitathakkar@users.noreply.github.com>
2025-09-20 14:00:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.java
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.java
@@ -1,40 +1,37 @@
 class Solution {
     public long minimumCost(String source, String target, char[] original, char[] changed, int[] cost) {
         int[][] matrix = new int[26][26];
-        for (int i = 0; i < 26; i++) {
-            Arrays.fill(matrix[i], Integer.MAX_VALUE);
+        for(int i=0;i<26;i++){
+            Arrays.fill(matrix[i],Integer.MAX_VALUE);
             matrix[i][i] = 0;
         }
-        for (int i = 0; i < cost.length; i++) {
-            matrix[original[i] - 'a'][changed[i] - 'a'] = Math.min(matrix[original[i] - 'a'][changed[i] - 'a'],
-                    cost[i]);
-           // System.out.println( matrix[original[i] - 'a'][changed[i] - 'a']);
+        for(int i=0;i<cost.length;i++){
+            matrix[original[i]-'a'][changed[i]-'a'] = cost[i];
         }
 
-        for (int k = 0; k < 26; k++) {
-            for (int i = 0; i < 26; i++) {
-                if (matrix[i][k] < Integer.MAX_VALUE) {
-                    for (int j = 0; j < 26; j++) {
-                        if (matrix[k][j] < Integer.MAX_VALUE) {
-                            matrix[i][j] = Math.min(matrix[i][k] + matrix[k][j], matrix[i][j]);
-                           // System.out.print(matrix[i][j] + " ");
-                        }
+        for(int k=0;k<26;k++){
+            for(int i=0;i<26;i++){
+                if(matrix[i][k]<Integer.MAX_VALUE){
+                    for(int j = 0;j<26;j++){
+                    if(matrix[k][j]<Integer.MAX_VALUE){
+                        matrix[i][j] = Math.min(matrix[i][j],matrix[i][k]+matrix[k][j]);
                     }
-                   // System.out.println();
+
                 }
+                }
+                
             }
         }
         long ans = 0;
-        for (int i = 0; i < source.length(); i++) {
+        for(int i=0;i<source.length();i++){
             int s = source.charAt(i) - 'a';
             int t = target.charAt(i) - 'a';
-            if (matrix[s][t] == Integer.MAX_VALUE) {
+            if(matrix[s][t] == Integer.MAX_VALUE){
                 return -1;
             }
-            else{
-            ans = ans + (long) matrix[s][t];
-            }
+            ans += matrix[s][t];
         }
+
         return ans;
     }
 }
