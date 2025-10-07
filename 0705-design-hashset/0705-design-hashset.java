caecd35c1565b276f928d9dd2d0b81aef7d08b0b
Dakshita Thakkar <55482291+dakshitathakkar@users.noreply.github.com>
2025-10-08 00:07:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.java
+++ 0705-design-hashset/0705-design-hashset.java
@@ -1,20 +1,58 @@
+class Node{
+    int val;
+    Node next;
+    public Node(int val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
 class MyHashSet {
-    List<Integer> list;
+    Node head;
     public MyHashSet() {
-        list = new ArrayList<>();
+        head = null;
     }
     
     public void add(int key) {
-        if(!contains(key))
-        list.add(key);
+        if(head == null){
+            head = new Node(key);
+            return;
+        }
+
+        if(!contains(key)){
+            Node temp = head;
+            while(temp.next != null){
+            temp = temp.next;
+            }
+            temp.next = new Node(key);
+        }
     }
     
     public void remove(int key) {
-        list.remove(Integer.valueOf(key));
+        if(head == null) return;
+        if(head != null && head.val == key){
+            head = null;
+            return;
+        }
+        Node temp = head;
+        while(temp.next != null){
+            if(temp.next.val == key){
+                temp.next = temp.next.next;
+                return;
+            }
+            temp = temp.next;
+        }
     }
     
     public boolean contains(int key) {
-        return list.contains(key);
+        Node temp = head;
+        while(temp != null){
+            if(temp.val == key){
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
     }
 }
 
