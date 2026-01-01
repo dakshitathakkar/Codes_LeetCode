5cb13c25bb571baa6784c3d77c35cc3dd85415e7
Dakshita Thakkar <55482291+dakshitathakkar@users.noreply.github.com>
2026-01-02 01:05:32 +0530

    Time: 2 ms (3.11%), Space: 43.8 MB (7.68%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int len = digits.length;
+        int[] res = new int[len+1];
+           List<Integer> arr = new ArrayList<>();
+        //909
+        //910
+        if(digits[len-1] != 9){
+            digits[len-1] = digits[len-1]+1;
+            return digits;
+        }
+        else{
+           boolean flag = false;
+           for(int i=len-1;i>=0;i--){
+                if(digits[i] == 9 && !flag){
+                    arr.add(0,0);
+                    
+                    if(i!=0){
+                        if(digits[i-1]!=9){
+                            arr.add(0,digits[i-1]+1);
+                            i--;
+                            flag = true;
+                        }
+                    }  
+                    
+                }
+                else if(!flag){
+                    arr.add(0,digits[i]+1);
+                }
+                else{
+                    arr.add(0,digits[i]);
+                }
+           }
+           if(arr.get(0) == 0){
+            arr.add(0,1);
+           }
+            res = arr.stream().mapToInt(i -> i).toArray();
+
+           System.out.println(arr);
+            
+        }
+        return res;
+    }
+}
